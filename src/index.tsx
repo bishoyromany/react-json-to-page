304c75322b2a2ae61c0d7015e4dcd50c3e331f28
Bishoy Romany <bishoy@queensenglish.co>
2022-12-07 14:17:36 +0200

    Add dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,10 @@
 import * as React from 'react'
+import './index.scss'
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+
 import styles from './styles.module.css'
 
 interface Props {
